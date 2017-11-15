83413c1ff5ab5acc4abb2b4fb5eead8c5a2b56f1
PokeHuntTyas <33252470+PokeHuntTyas@users.noreply.github.com>
2017-11-15 22:59:14 +0100

    Update checker.py

--- proxybroker/checker.py
+++ proxybroker/checker.py
@@ -140,7 +140,7 @@ class Checker:
             else:
                 result = await self._check(proxy, proto)
             results.append(result)
-        r = requests.get("https://pgorelease.nianticlabs.com/plfe/version", proxies={'http': ('http://' + str(proxy.host)})
+        r = requests.get("https://pgorelease.nianticlabs.com/plfe/version", proxies={'http': ('http://' + str(proxy.host))})
         r2 = requests.get("https://sso.pokemon.com/sso/login", proxies={'http': ('http://' + str(proxy.host)})
         proxy.is_working = True if any(results) else False
 
